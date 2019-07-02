27675e568710b477d72b19b8218b8a47c0e580b1
Christian Schmitt <c.schmitt@envisia.de>
2019-07-02 12:06:34 +0200

    added all hawk related files from HawkNet

---
+++ Envisia.Hawk/HawkCredential.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Envisia.Hawk
+{
+    /// <summary>
+    /// Contains private information about an user
+    /// </summary>
+    public class HawkCredential
+    {
+        public HawkCredential()
+        {
+            User = string.Empty;
+        }
+
+        /// <summary>
+        /// Key Id
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Symmetric Key
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Hashing Algorithm
+        /// </summary>
+        public string Algorithm { get; set; }
+
+        /// <summary>
+        /// User name
+        /// </summary>
+        public string User { get; set; }
+
+        /// <summary>
+        /// Additional Claims
+        /// </summary>
+        public Claim[] AdditionalClaims { get; set; }
+
+        /// <summary>
+        /// User roles
+        /// </summary>
+        public string[] Roles { get; set; }
+#endif
+    }
+}
